89e3fae58ac89bb757492263560b9ddf389f0ead
Ankita Singh <2911ankitadev@gmail.com>
2024-11-23 04:05:12 +0530

    commit

--- app.js
+++ app.js
@@ -58,6 +58,14 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://chitchat-frontend-opal.vercel.app'); // Frontend origin
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/chat", chatRoute);
 app.use("/api/v1/admin", adminRoute);
